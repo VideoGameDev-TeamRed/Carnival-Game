53a5919da0ac1bdb4f4a2ef59426db333ab7f8b0
Aaron Weaver <waaronl@okstate.edu>
2016-03-08 21:26:04 -0600

    Deliver me from this torment, oh Unity gods, for I have given up.

--- Assets/Scripts/Cannon.cs
+++ Assets/Scripts/Cannon.cs
@@ -26,22 +26,28 @@ public class Cannon : IRotateable, IShootable {
 
 		Debug.Log ("x: " + eulerAngles.x + ",y: " + eulerAngles.y + ",z: " + eulerAngles.z);
 
-		if (eulerAngles.y >= 120 && eulerAngles.y <= 240) {
-			//cylinder.Rotate (new Vector3 (turnSpeed, 0, 0));
-			CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
-		}
+//		Quaternion rotationQuat = Quaternion.Euler (turnSpeed, 0f, 0f);
+//
+//		rotationQuat = this.ClampRotationAroundYAxis (rotationQuat, 300f, 355f);
+//
+//		cylinder.localRotation = rotationQuat;
 
-		if (eulerAngles.y >= 240 && turnSpeed < 0) {
-			//cylinder.Rotate (new Vector3 (turnSpeed, 0, 0));
-			CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
-		}
+//		if (eulerAngles.y >= 120 && eulerAngles.y <= 240) {
+//			//cylinder.Rotate (new Vector3 (turnSpeed, 0, 0));
+//			CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
+//		}
+//
+//		if (eulerAngles.y >= 240 && turnSpeed < 0) {
+//			//cylinder.Rotate (new Vector3 (turnSpeed, 0, 0));
+//			CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
+//		}
+//
+//		if (eulerAngles.y <= 120 && turnSpeed > 0) {
+//			//cylinder.Rotate (new Vector3 (turnSpeed, 0, 0));
+//			CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
+//		}
 
-		if (eulerAngles.y <= 120 && turnSpeed > 0) {
-			//cylinder.Rotate (new Vector3 (turnSpeed, 0, 0));
-			CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
-		}
-
-		//CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
+		CannonObject.transform.Rotate(new Vector3(0, turnSpeed, 0));
 	}
 
 	public void RotateVertical (float turnSpeed) {
@@ -50,22 +56,45 @@ public class Cannon : IRotateable, IShootable {
 		// Finds the subview of the Cannon that is the cylinder.
 		// This Script is to be used with the lCannon prefab, so Cylinder will never be null.
 		Transform cylinder = CannonObject.transform.Find("Cannon/Cylinder");
+//
+//		float angle = Mathf.Clamp (turnSpeed, -180, 180);
+//
+//		cylinder.localEulerAngles = new Vector3 (cylinder.localEulerAngles.x, cylinder.localEulerAngles.y, angle);
 
-		Vector3 eulerAngles = cylinder.eulerAngles;
+//		Vector3 angle = cylinder.localEulerAngles;
+//		angle.x = Mathf.Clamp(angle.x * Input.GetAxis("Mouse Y"), 0.0f, -50.0f);
+//		Debug.Log ("X: " + angle.x);
+//		cylinder.localEulerAngles = angle;
+
+//		Quaternion rotationQuat = cylinder.localRotation;
+//		rotationQuat *= Quaternion.Euler (0f, turnSpeed, 0f);
+//
+//		rotationQuat = ClampRotationAroundYAxis (rotationQuat, 300, 355);
+		//rotationZ = Mathf.Clamp (rotationZ, -90, 90);
+
+		//rotationQuat = this.ClampRotationAroundYAxis (rotationQuat, 300f, 355f);
+
+		//cylinder.localRotation = rotationQuat;
 		//Debug.Log ("x: " + eulerAngles.x + ",y: " + eulerAngles.y + ",z: " + eulerAngles.z);
 
 
-		if (eulerAngles.x >= 300 && eulerAngles.x <= 355) {
-			cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
-		}
-
-		if (eulerAngles.x >= 355 && turnSpeed > 0) {
-			cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
-		}
-
-		if (eulerAngles.x <= 300 && turnSpeed < 0) {
-			cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
-		}
+//		if (eulerAngles.x >= 300 && eulerAngles.x <= 355) {
+//			cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
+//		}
+//
+//		if (eulerAngles.x >= 355) {
+//			//cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
+//			eulerAngles.x = 349.999f;
+//			cylinder.eulerAngles = eulerAngles;
+//		}
+//
+//		if (eulerAngles.x <= 300) {
+//			//cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
+//			eulerAngles.x = 300.0001f;
+//			cylinder.eulerAngles = eulerAngles;
+//		}
+		cylinder.Rotate (new Vector3 (0, turnSpeed, 0));
+		//rigid.MoveRotation (Quaternion.EulerAngles (cylinder.eulerAngles));
 	}
 
 	public void Fire (float shotForce) {
@@ -84,5 +113,33 @@ public class Cannon : IRotateable, IShootable {
 		// Wait for timeToReload
 		yield return new WaitForSeconds(timeToReload);
 		IsLoaded = true;
+	}
+
+	private float ClampAngle(float angle, float min, float max) {
+
+		if (angle<90 || angle>270){       // if angle in the critic region...
+			if (angle>180) angle -= 360;  // convert all angles to -180..+180
+			if (max>180) max -= 360;
+			if (min>180) min -= 360;
+		}    
+		angle = Mathf.Clamp(angle, min, max);
+		if (angle<0) angle += 360;  // if angle negative, convert to 0..360
+		return angle;
+	}
+
+	private Quaternion ClampRotationAroundYAxis(Quaternion q, float min, float max)
+	{
+		q.x /= q.w;
+		q.y /= q.w;
+		q.z /= q.w;
+		q.w = 1.0f;
+
+		float angleY = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+
+		angleY = Mathf.Clamp (angleY, min, max);
+
+		q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleY);
+
+		return q;
 	}
 }
